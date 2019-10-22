417ed74213c98f1cfc1c6c1a540457cbe933fa61
Kunn Liu <abce1202@gmail.com>
2019-10-22 19:45:29 +0800

    find file

--- products.py
+++ products.py
@@ -1,18 +1,25 @@
 
+import os # operating system
+
 #讀取檔案
 products = []
-with open('products.csv', 'r', encoding = "utf-8") as f:
-	for line in f:
-		# s = line.strip().split(',') #切割split，裡面放你想要用什麼來分割，這邊是 "，"；strip是去掉 "\n"
-		# name = s[0]
-		# price = s[1]
-		if '商品,價格' in line:
-			continue #continue是跳到下一回的意思
-		name, price = line.strip().split(',')
-		products.append([name, price])
-		print([name, price])
-		# # split 完的結果是 清單 list
+if os.path.isfile('products.csv'):
+	print('yeah!!程式啟動~~')
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			# s = line.strip().split(',') #切割split，裡面放你想要用什麼來分割，這邊是 "，"；strip是去掉 "\n"
+			# name = s[0]
+			# price = s[1]
+			if '商品,價格' in line:
+				continue #continue是跳到下一回的意思
+			name, price = line.strip().split(',')
+			products.append([name, price])
+			print([name, price])
+			# # split 完的結果是 清單 list
+else:
+	print('找不到檔案......')
 
+#讓使用者輸入新的商品		
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -24,11 +31,12 @@ while True:
 	# p = [name, price]
 	# products.append(p)
 	products.append([name, price])
+#列出清單
 print(products)
-
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格' + '\n')
 	for p in products:
