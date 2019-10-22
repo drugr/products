26ee371be7f3dbfc7ebdb10085040850396c5913
Kunn Liu <abce1202@gmail.com>
2019-10-22 19:19:06 +0800

    read file

--- products.py
+++ products.py
@@ -1,5 +1,18 @@
 
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding = "utf-8") as f:
+	for line in f:
+		# s = line.strip().split(',') #切割split，裡面放你想要用什麼來分割，這邊是 "，"；strip是去掉 "\n"
+		# name = s[0]
+		# price = s[1]
+		if '商品,價格' in line:
+			continue #continue是跳到下一回的意思
+		name, price = line.strip().split(',')
+		products.append([name, price])
+		print([name, price])
+		# # split 完的結果是 清單 list
+
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
